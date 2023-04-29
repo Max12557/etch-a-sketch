c35e0c3c2a3d7d5644e18967e10c8c232ceabe71
Maxs <maksim.02.kushpa@gmail.com>
2023-04-29 12:51:44 +0300

    create a grid and add mouseetner listener to every box for change the background color

--- main.js
+++ main.js
@@ -1,1 +1,36 @@
+const sizeOfGrid = 32;
+
 const canvas = document.querySelector('#canvas');
+
+const createGrid = (amtOfGrips) => {
+    for (let i = 0; i < amtOfGrips; i++) {
+        const row = document.createElement('div');
+        row.classList.add('grid-row');
+        
+        for (let j = 0; j < amtOfGrips; j++) {
+            const gridBox = document.createElement('div');
+            const widthAndHeight = 800 / sizeOfGrid;
+            gridBox.style.width = `${widthAndHeight}px`;
+            gridBox.style.height = `${widthAndHeight}px`;
+            gridBox.classList.add('grid-box');
+
+            // adding mouseenter to change the background color
+            gridBox.addEventListener('mouseenter', () => {
+                gridBox.style.backgroundColor = 'black';
+            })
+
+            row.appendChild(gridBox);
+        }
+
+        canvas.append(row);
+    }
+}
+
+createGrid(sizeOfGrid);
+
+// const allDivs = document.querySelectorAll('.grid-box');
+// allDivs.forEach(div => {
+//     div.addEventListener('mouseenter', () => {
+//         div.style.backgroundColor = 'black';
+//     })
+// })
